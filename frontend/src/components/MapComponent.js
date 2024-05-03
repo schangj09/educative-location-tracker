cea7a2661af2317d6397902c5a3e14772daca439
Jeffrey Schang <schangj09@gmail.com>
2024-05-03 14:09:58 +0000

    Task 2 - add the react-leaflet MapContainer and TileLayer to the AddLocation page.

--- frontend/src/components/MapComponent.js
+++ frontend/src/components/MapComponent.js
@@ -1,5 +1,19 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 
+const center = [39.5834, 2.6462];
+const zoom = 12;
+
 export default function MapComponent() {
-  return <div>MapComponent</div>;
+  return (
+    <div className="w-full overflow-hidden relative z-0 h-screen">
+      <MapContainer placeholder zoom={zoom} center={center} scrollWheelZoom={false} className="h-[90%] w-auto leaflet-container">
+        <TileLayer
+         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+         >
+
+        </TileLayer>
+      </MapContainer>
+    </div>
+  );
 }
--- frontend/src/pages/AddLocation.js
+++ frontend/src/pages/AddLocation.js
@@ -1,19 +1,23 @@
-import Autocomplete from '../components/Autocomplete';
-import MapComponent from '../components/MapComponent';
-import useAuth from '../hooks/useAuth';
-import axios from 'axios';
+import Autocomplete from "../components/Autocomplete";
+import MapComponent from "../components/MapComponent";
+import useAuth from "../hooks/useAuth";
+import axios from "axios";
 import { toast } from "react-toastify";
-import Cookies from 'js-cookie';
-import serverURL from '../utils/urls';
-import { useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import AppContext from '../utils/AppContext';
+import Cookies from "js-cookie";
+import serverURL from "../utils/urls";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import AppContext from "../utils/AppContext";
 
 export default function AddLocation() {
   useAuth();
   return (
     <div className="text-black dark:text-white lg:p-10">
-      Add Location Page
+      <div className="h-screen overflow-hidden left-0 fixed w-full">
+        <div className="absoulte top-0 h-full">
+          <MapComponent />
+        </div>
+      </div>
     </div>
   );
 }
