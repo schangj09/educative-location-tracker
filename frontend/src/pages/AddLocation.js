3fb0ee7b76058c8be45d525bef02e8d026c72e1c
Jeffrey Schang <schangj09@gmail.com>
2024-05-13 19:39:16 +0200

    pass onAddLocation prop from the AddLocation page

--- frontend/src/pages/AddLocation.js
+++ frontend/src/pages/AddLocation.js
@@ -10,13 +10,21 @@ import { useContext } from "react";
 import AppContext from "../utils/AppContext";
 
 export default function AddLocation() {
+
+  // Handle add current location
+  const handleAddLocation = (name, latitude, longitude) => {
+    console.log(`name=${name}`)
+    console.log(`latitude=${latitude}`)
+    console.log(`longitude=${longitude}`)
+  };
+
   useAuth();
   return (
     <div>
       <div className="h-screen  overflow-hidden left-0 fixed w-full">
         <div className="fixed lg:absolute w-full mt-20  z-40 flex flex-col justify-center items-center">
           <div className="w-[400px]">
-            <Autocomplete />
+            <Autocomplete onAddLocation={handleAddLocation} />
           </div>
         </div>
         <div className="absoulte top-0 h-full">
